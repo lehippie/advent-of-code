2455836b5336bba370803706e2170d313352ed5d
Thomas Bedetti <thomas.bedetti@withings.com>
2019-12-20 11:10:34 +0100

    day03 solutions

--- days/day03.py
+++ days/day03.py
@@ -1,15 +1,17 @@
 """Day 3 script."""
 
 import env
-from lib import wires_panel as wp
+from lib import wires
 
 # Input
 with open('inputs/03_wires.txt') as f:
-    wire1_path = f.readline()
-    wire2_path = f.readline()
+    wire1 = wires.Wire(f.readline())
+    wire2 = wires.Wire(f.readline())
 
-# Part 1
-distance = wp.distance(wire1_path, wire2_path)
-print(f"Manhattan distance is: {distance}")
+# Part 1: 446
+distance = wire1.closest_cross(wire2)
+print(f"Closest intersection is: {distance}")
 
-# Part 2
+# Part 2: 9006
+timing = wire1.shortest_cross(wire2)
+print(f"Shortest intersection is: {timing}")
