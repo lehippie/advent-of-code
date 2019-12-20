3164776754271afac0970488e680ed64380e8251
Thomas Bedetti <thomas.bedetti@withings.com>
2019-12-20 09:16:05 +0100

    added solutions to day01 and day02

--- days/day01.py
+++ days/day01.py
@@ -3,16 +3,19 @@
 import env
 from lib import fuel
 
+
 # Input
 modules_masses = []
 with open('inputs/01_modules.txt') as f:
     for mass in f:
         modules_masses.append(int(mass))
 
-# Part 1
+
+# Part 1: 3426455
 fuel_modules = [fuel.fuel_for_mass(m) for m in modules_masses]
 print(f"Amount of fuel needed for the modules: {sum(fuel_modules)}")
 
-# Part 2
+
+# Part 2: 5136807
 total_fuel = [fuel.fuel_total(m) for m in modules_masses]
 print(f"Amount of fuel needed, counting added fuel: {sum(total_fuel)}")
--- days/day02.py
+++ days/day02.py
@@ -3,17 +3,19 @@
 import env
 from lib import intcode
 
-# Input
-gap_file = 'inputs/02_gravity_assist_program.txt'
 
-# Part 1
-gap = intcode.from_file(gap_file)
+# Input
+gap = intcode.from_file('inputs/02_gravity_assist_program.txt')
+
+
+# Part 1: 4484226
 gap.memory[1] = 12
 gap.memory[2] = 2
 gap.execute()
 print(f"Gravity Assist Program output for '1202' input: {gap.memory[0]}")
 
-# Part 2
+
+# Part 2: 5696
 def execute(program, noun, verb):
     """Execute program with given noun and verb."""
     program.reset()
@@ -32,14 +34,18 @@ def find_input(program, wanted_output, max_input=100):
 wanted = 19690720
 print(f"Input giving {wanted} output: {find_input(gap, wanted)}")
 
+
 # Extra
-import numpy as np
-import matplotlib.pyplot as plt
-outputs = np.zeros((100, 100))
-nouns = list(range(100))
-verbs = nouns.copy()
-for n in nouns:
-    for v in verbs:
-        outputs[n,v] = execute(gap, n, v)
-plt.imshow(outputs)
-plt.show()
+def gap_outputs():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    outputs = np.zeros((100, 100))
+    nouns = list(range(100))
+    verbs = nouns.copy()
+    for n in nouns:
+        for v in verbs:
+            outputs[n,v] = execute(gap, n, v)
+    plt.imshow(outputs)
+    plt.show()
+
+# gap_outputs()
