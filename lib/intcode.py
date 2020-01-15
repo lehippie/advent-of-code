9002d38cd09f6983ceadf4a34b25768b41525134
Thomas Bedetti <thomas.bedetti@withings.com>
2020-01-15 18:15:47 +0100

    handle multiple inputs

--- lib/intcode.py
+++ lib/intcode.py
@@ -14,7 +14,7 @@ class Intcode():
 
     def __init__(self, init_values):
         """Initialize memory state."""
-        self.init_values = init_values
+        self.initial_memory = init_values
         self.memory = init_values.copy()
         self.pointer = 0
         self.finished = False
@@ -28,11 +28,11 @@ class Intcode():
 
     def reset(self):
         """Restore Program to initial state."""
-        self.__init__(self.init_values)
+        self.__init__(self.initial_memory)
 
-    def execute(self, input_value=None):
+    def execute(self, input_values=None):
         """Execute the whole program."""
-        self.input = input_value
+        self.input = input_values
         while not self.finished:
             self.execute_next()
         return self.output
@@ -41,34 +41,24 @@ class Intcode():
         """Execute the next instruction."""
         self.instr = self.memory[self.pointer]
         opcode = self.instr % 100
-
         if opcode == 1:
-            self.add(self.process_parameters(2))
-
+            self._instr_add(self.process_parameters(2))
         elif opcode == 2:
-            self.multiply(self.process_parameters(2))
-
+            self._instr_multiply(self.process_parameters(2))
         elif opcode == 3:
-            self.get_input()
-
+            self._instr_input()
         elif opcode == 4:
-            self.output_value(self.process_parameters(1))
-
+            self._instr_output(self.process_parameters(1))
         elif opcode == 5:
-            self.jump_if_true(self.process_parameters(2))
-
+            self._instr_jump_if_true(self.process_parameters(2))
         elif opcode == 6:
-            self.jump_if_false(self.process_parameters(2))
-
+            self._instr_jump_if_false(self.process_parameters(2))
         elif opcode == 7:
-            self.less_than(self.process_parameters(2))
-
+            self._instr_less_than(self.process_parameters(2))
         elif opcode == 8:
-            self.equals(self.process_parameters(2))
-
+            self._instr_equals(self.process_parameters(2))
         elif opcode == 99:
             self.finished = True
-
         else:
             raise IOError(
                 f"Unknown opcode '{opcode}' at address {self.pointer}.")
@@ -89,28 +79,31 @@ class Intcode():
     ####    INSTRUCTIONS    ####
     ############################
 
-    def add(self, parameters):
+    def _instr_add(self, parameters):
         """Addition instruction (opcode 1)."""
         result_address = self.memory[self.pointer + 3]
         self.memory[result_address] = parameters[0] + parameters[1]
         self.pointer += 4
 
-    def multiply(self, parameters):
+    def _instr_multiply(self, parameters):
         """Multiplication instruction (opcode 2)."""
         result_address = self.memory[self.pointer + 3]
         self.memory[result_address] = parameters[0] * parameters[1]
         self.pointer += 4
 
-    def get_input(self):
+    def _instr_input(self):
         """Input instruction (opcode 3)."""
         result_address = self.memory[self.pointer + 1]
         if self.input is None:
             self.memory[result_address] = int(input("Enter input value: "))
-        else:
+        elif isinstance(self.input, int):
             self.memory[result_address] = self.input
+        else:
+            self.memory[result_address] = self.input[0]
+            self.input = self.input[1:]
         self.pointer += 2
 
-    def output_value(self, parameter):
+    def _instr_output(self, parameter):
         """Output instruction (opcode 4)."""
         if self.output is None:
             self.output = parameter
@@ -120,27 +113,27 @@ class Intcode():
             self.output.append(parameter)
         self.pointer += 2
 
-    def jump_if_true(self, parameters):
+    def _instr_jump_if_true(self, parameters):
         """Jump-if-true instruction (opcode 5)."""
         if parameters[0]:
             self.pointer = parameters[1]
         else:
             self.pointer += 3
 
-    def jump_if_false(self, parameters):
+    def _instr_jump_if_false(self, parameters):
         """Jump-if-false instruction (opcode 6)."""
         if not parameters[0]:
             self.pointer = parameters[1]
         else:
             self.pointer += 3
 
-    def less_than(self, parameters):
+    def _instr_less_than(self, parameters):
         """Comparison instruction (opcode 7)."""
         result_address = self.memory[self.pointer + 3]
         self.memory[result_address] = int(parameters[0] < parameters[1])
         self.pointer += 4
 
-    def equals(self, parameters):
+    def _instr_equals(self, parameters):
         """Equality instruction (opcode 8)."""
         result_address = self.memory[self.pointer + 3]
         self.memory[result_address] = int(parameters[0] == parameters[1])
