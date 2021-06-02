40b3f18ca7cada8bd1492a15e8a8009a193f0752
lehippie <dahip@gmx.fr>
2021-06-02 12:10:42 +0200

    [aoc.puzzle] test and solve logic

--- aoc/puzzle.py
+++ aoc/puzzle.py
@@ -1,10 +1,12 @@
 """Base puzzle class."""
 
+
 class Puzzle:
-    def __init__(self, puzzle_input=None, solution_one=None, solution_two=None):
-        self.input = puzzle_input
+    def __init__(self, tests=None, solution_one=None, solution_two=None):
+        self.tests = tests
         self.solution_one = solution_one
         self.solution_two = solution_two
+        self.input = None
 
     def part_one(self):
         return NotImplemented
@@ -12,15 +14,38 @@ class Puzzle:
     def part_two(self):
         return NotImplemented
 
+    def test(self):
+        """Peform test checks."""
+        parts = (self.part_one, self.part_two)
+        for p, part in enumerate(self.tests):
+            for test, solution in self.tests[part]:
+                self.input = test
+                answer = parts[p]()
+                if not answer == solution:
+                    print("Test failed in", part)
+                    print(f"{test} gives {answer} instead of {solution}.")
+                    return False
+        print("Tests passed!")
+        return True
+
     def solve(self):
+        """Run puzzle parts."""
         answer_one = self.part_one()
         if self.solution_one is None:
             print("Part One answer:", answer_one, "?")
+        elif answer_one != self.solution_one:
+            print(
+                "Regression in part one:",
+                f"got {answer_one} instead of {self.solution_one}.",
+            )
         else:
-            assert answer_one == self.solution_one
             answer_two = self.part_two()
             if self.solution_two is None:
                 print("Part Two answer:", answer_two, "?")
+            elif answer_two != self.solution_two:
+                print(
+                    "Regression in part two:",
+                    f"got {answer_two} instead of {self.solution_two}.",
+                )
             else:
-                assert answer_two == self.solution_two
                 print("Day completed \o/")
