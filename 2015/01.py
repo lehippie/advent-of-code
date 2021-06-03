1d7b5091e3e377293f1ea7e7304149cc6d2019b6
lehippie <dahip@gmx.fr>
2021-06-03 13:02:41 +0200

    2015.01 using aoc module

--- 2015/01.py
+++ 2015/01.py
@@ -1,25 +1,11 @@
 """Day 1: Not Quite Lisp."""
 
-from pathlib import Path
 from collections import Counter
 
-
-PUZZLE_INPUT_FILE = "inputs/01.txt"
-ANSWER_PART_ONE = 74
-ANSWER_PART_TWO = 1795
+from aoc.puzzle import Puzzle
 
 
-class Puzzle:
-    def __init__(self, puzzle_input=""):
-        self.input = puzzle_input
-
-    @classmethod
-    def from_file(cls, filename):
-        filepath = Path(__file__).parent / filename
-        with open(filepath) as f:
-            puzzle_input = f.readline().strip()
-        return cls(puzzle_input)
-
+class TodayPuzzle(Puzzle):
     def part_one(self):
         parentheses = Counter(self.input)
         return parentheses["("] - parentheses[")"]
@@ -33,38 +19,25 @@ class Puzzle:
         return i + 1
 
 
-def tests():
-    assert Puzzle("(())").part_one() == 0
-    assert Puzzle("()()").part_one() == 0
-    assert Puzzle("(((").part_one() == 3
-    assert Puzzle("(()(()(").part_one() == 3
-    assert Puzzle("))(((((").part_one() == 3
-    assert Puzzle("())").part_one() == -1
-    assert Puzzle("))(").part_one() == -1
-    assert Puzzle(")))").part_one() == -3
-    assert Puzzle(")())())").part_one() == -3
-    assert Puzzle(")").part_two() == 1
-    assert Puzzle("()())").part_two() == 5
-
-
-def solve(puzzle_input, answer_one, answer_two):
-    puzzle = Puzzle.from_file(puzzle_input)
-    result_one = puzzle.part_one()
-    if answer_one is None:
-        print("Part One answer:", result_one, "?")
-    else:
-        assert result_one == answer_one
-        result_two = puzzle.part_two()
-        if answer_two is None:
-            print("Part Two answer:", result_two, "?")
-        else:
-            assert result_two == answer_two
-            print("Day completed \o/")
-
-
 if __name__ == "__main__":
-    tests()
-    if PUZZLE_INPUT_FILE:
-        solve(PUZZLE_INPUT_FILE, ANSWER_PART_ONE, ANSWER_PART_TWO)
-    else:
-        print("No input given.")
+    puzzle = TodayPuzzle(
+        tests={
+            "part_one": [
+                ("(())", 0),
+                ("()()", 0),
+                ("(((", 3),
+                ("(()(()(", 3),
+                ("))(((((", 3),
+                ("())", -1),
+                ("))(", -1),
+                (")))", -3),
+                (")())())", -3),
+            ],
+            "part_two": [(")", 1), ("()())", 5)],
+        },
+        solution_one=74,
+        solution_two=1795,
+    )
+
+    if puzzle.test():
+        puzzle.solve()
