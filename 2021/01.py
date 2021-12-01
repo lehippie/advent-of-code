0c96f9c16ea2dcc5d87d82c9a56768c618e0100e
lehippie <dahip@gmx.fr>
2021-12-01 14:53:17 +0100

    2021.01

---
+++ 2021/01.py
@@ -0,0 +1,18 @@
+"""Day 1: Sonar Sweep."""
+
+from aoc.puzzle import Puzzle
+
+
+class Puzzle01(Puzzle):
+    def parser(self):
+        return list(map(int, self.input))
+
+    def part_one(self, n=1):
+        return sum(x < y for x, y in zip(self.input, self.input[n:]))
+
+    def part_two(self):
+        return self.part_one(3)
+
+
+if __name__ == "__main__":
+    Puzzle01(solutions=(1448, 1471)).solve()
