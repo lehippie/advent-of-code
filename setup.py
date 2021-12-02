86abed30b6244ea408a7135a0c51f42da889edf8
lehippie <dahip@gmx.fr>
2021-12-02 15:22:39 +0100

    Clean repo from unused test files

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="aoc",
     version="0.1.0",
     description="Advent of Code Tools",
-    packages=find_packages(),
+    packages=["aoc"],
 )
