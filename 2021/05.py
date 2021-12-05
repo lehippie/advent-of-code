b278fc82703c443ec4c8fd2b71ea66ce8a8de280
lehippie <dahip@gmx.fr>
2021-12-05 19:40:04 +0100

    2021.05

---
+++ 2021/05.py
@@ -0,0 +1,45 @@
+"""Day 5: Hydrothermal Venture."""
+
+import re
+from collections import Counter
+from aoc.puzzle import Puzzle
+
+
+class Puzzle05(Puzzle):
+    def parser(self):
+        self.straights = []
+        self.diagonals = []
+        for line in self.input:
+            x1, y1, x2, y2 = map(int, re.findall(r"\d+", line))
+            if x1 == x2 or y1 == y2:
+                self.straights.append(((x1, y1), (x2, y2)))
+            else:
+                self.diagonals.append(((x1, y1), (x2, y2)))
+
+    def part_one(self):
+        count = Counter()
+        for (x1, y1), (x2, y2) in self.straights:
+            x1, x2 = sorted((x1, x2))
+            y1, y2 = sorted((y1, y2))
+            count.update(
+                (x, y) for x in range(x1, x2 + 1) for y in range(y1, y2 + 1)
+            )
+        return sum(c > 1 for c in count.values())
+
+    def part_two(self):
+        count = Counter()
+        for (x1, y1), (x2, y2) in self.straights:
+            x1, x2 = sorted((x1, x2))
+            y1, y2 = sorted((y1, y2))
+            count.update(
+                (x, y) for x in range(x1, x2 + 1) for y in range(y1, y2 + 1)
+            )
+        for (x1, y1), (x2, y2) in self.diagonals:
+            rx = range(x1, x2 + 1) if x1 < x2 else range(x1, x2 - 1, -1)
+            ry = range(y1, y2 + 1) if y1 < y2 else range(y1, y2 - 1, -1)
+            count.update((x, y) for x, y in zip(rx, ry))
+        return sum(c > 1 for c in count.values())
+
+
+if __name__ == "__main__":
+    Puzzle05(solutions=(5585, 17193)).solve()
