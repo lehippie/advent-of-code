4d3f60f3221ebd252dbd27bb39223559d80fbcad
lehippie <dahip@gmx.fr>
2021-12-06 13:26:59 +0100

    [2021.06] just learned about str's count method

--- 2021/06.py
+++ 2021/06.py
@@ -1,16 +1,11 @@
 """Day 6: Lanternfish."""
 
-from collections import Counter
 from aoc.puzzle import Puzzle
 
 
 class Puzzle06(Puzzle):
-    def parser(self):
-        self.lanternfishes = list(map(int, self.input.split(",")))
-
     def part_one(self, days=80):
-        ages = Counter(self.lanternfishes)
-        ages = [ages[k] for k in range(9)]
+        ages = [self.input.count(str(k)) for k in range(9)]
         for _ in range(days):
             ages = ages[1:] + [ages[0]]
             ages[6] += ages[-1]
