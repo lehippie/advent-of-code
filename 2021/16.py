02bf18b3698b8906d5e4b7061ede43fa0cfa9b4a
lehippie <dahip@gmx.fr>
2021-12-16 15:25:14 +0100

    2021.16

---
+++ 2021/16.py
@@ -0,0 +1,84 @@
+"""Day 16: Packet Decoder."""
+
+from math import prod
+from aoc.puzzle import Puzzle
+
+
+class Packet:
+    def __init__(self, binary):
+        self.bin = binary
+        self.version = int(self.bin[0:3], 2)
+        self.id = int(self.bin[3:6], 2)
+        self.content = self.decode_value() if self.id == 4 else self.decode_op()
+
+    def decode_value(self):
+        value = ""
+        k = 6
+        while True:
+            group = self.bin[k : k + 5]
+            value += group[1:]
+            k += 5
+            if group[0] == "0":
+                break
+        self.size = k
+        return int(value, 2)
+
+    def decode_op(self):
+        sub_type = self.bin[6]
+        content = []
+        if sub_type == "0":
+            self.size = 22 + int(self.bin[7:22], 2)
+            to_read = self.bin[22 : self.size]
+            while to_read:
+                content.append(Packet(to_read))
+                to_read = to_read[content[-1].size :]
+        else:
+            sub_count = int(self.bin[7:18], 2)
+            to_read = self.bin[18:]
+            for _ in range(sub_count):
+                content.append(Packet(to_read))
+                to_read = to_read[content[-1].size :]
+            self.size = 18 + sum(packet.size for packet in content)
+        return content
+
+    def vsum(self):
+        if self.id == 4:
+            return self.version
+        else:
+            return self.version + sum(packet.vsum() for packet in self.content)
+
+    @property
+    def value(self):
+        if self.id == 0:
+            return sum(packet.value for packet in self.content)
+        elif self.id == 1:
+            return prod(packet.value for packet in self.content)
+        elif self.id == 2:
+            return min(packet.value for packet in self.content)
+        elif self.id == 3:
+            return max(packet.value for packet in self.content)
+        elif self.id == 4:
+            return self.content
+        elif self.id == 5:
+            return 1 if self.content[0].value > self.content[1].value else 0
+        elif self.id == 6:
+            return 1 if self.content[0].value < self.content[1].value else 0
+        elif self.id == 7:
+            return 1 if self.content[0].value == self.content[1].value else 0
+
+
+class Puzzle16(Puzzle):
+    def parser(self):
+        return "".join(f"{int(f'0x{x}', 16):04b}" for x in self.input)
+
+    def part_one(self):
+        transmission = Packet(self.input)
+        return transmission.vsum()
+
+    def part_two(self):
+        transmission = Packet(self.input)
+        return transmission.value
+
+
+if __name__ == "__main__":
+    Puzzle16(solutions=(977, 101501020883)).solve()
