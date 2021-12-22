8894422b9d5947cb113e5283378205cbc661da52
lehippie <dahip@gmx.fr>
2021-12-22 13:26:05 +0100

    cleaner player amount support

--- 2021/21.py
+++ 2021/21.py
@@ -8,7 +8,8 @@ from aoc.puzzle import Puzzle
 class DiracDice:
     def __init__(self, starting_positions):
         self.positions = list(starting_positions)
-        self.scores = [0] * len(self.positions)
+        self.n_players = len(self.positions)
+        self.scores = [0] * self.n_players
         self.turn = 0
         self.dice = cycle(range(1, 101))
 
@@ -17,7 +18,7 @@ class DiracDice:
 
     def next_turn(self):
         roll = sum(next(self.dice) for _ in range(3))
-        player = self.turn % len(self.positions)
+        player = self.turn % self.n_players
         self.positions[player] = (self.positions[player] + roll) % 10 or 10
         self.scores[player] += self.positions[player]
         self.turn += 1
@@ -34,22 +35,23 @@ class Puzzle21(Puzzle):
         return min(game.scores) * 3 * game.turn
 
     def part_two(self):
+        n_players = len(self.input)
         quantum_rolls = Counter(sum(rolls) for rolls in product(range(1, 4), repeat=3))
-        wins = [0] * len(self.input)
-        games = {self.input + (0,) * len(wins) + (0,): 1}
+        wins = [0] * n_players
+        games = {self.input + (0,) * n_players + (0,): 1}
         while games:
             _games = defaultdict(int)
             for state, n_games in games.items():
                 player = state[-1]
                 for roll, n_roll in quantum_rolls.items():
-                    positions = list(state[0 : len(wins)])
-                    scores = list(state[len(wins) : -1])
+                    positions = list(state[0:n_players])
+                    scores = list(state[n_players:-1])
                     positions[player] = (positions[player] + roll) % 10 or 10
                     scores[player] += positions[player]
                     if scores[player] >= 21:
                         wins[player] += n_games * n_roll
                     else:
-                        _state = tuple(positions + scores + [(player + 1) % len(wins)])
+                        _state = tuple(positions + scores + [(player + 1) % n_players])
                         _games[_state] += n_games * n_roll
             games = _games
         return max(wins)
