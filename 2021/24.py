fdf945ad13d15cdb367e6af4915379d5a9f8c8e6
lehippie <dahip@gmx.fr>
2022-11-22 13:39:06 +0100

    2021.24 Extremely slow [1h]

--- 2021/24.py
+++ 2021/24.py
@@ -4,10 +4,12 @@ from aoc.puzzle import Puzzle
 
 
 class ALU:
-    def __init__(self, input_number, program):
-        self.inputs = (int(d) for d in str(input_number))
+    def __init__(self, inputs, program, z=None):
+        self.inputs = inputs[::-1] if isinstance(inputs, list) else [inputs]
         self.program = program
         self.mem = {"w": 0, "x": 0, "y": 0, "z": 0}
+        if z is not None:
+            self.mem["z"] = z
 
     @property
     def instructions(self):
@@ -21,7 +23,7 @@ class ALU:
         }
 
     def inp(self, var):
-        self.mem[var] = next(self.inputs)
+        self.mem[var] = self.inputs.pop()
 
     def add(self, var, value):
         self.mem[var] += value
@@ -41,28 +43,56 @@ class ALU:
     def run(self):
         for instruction in self.program:
             cmd, *v = instruction.split()
-            if cmd != "inp":
+            if len(v) > 1:
                 v[1] = int(self.mem.get(v[1], v[1]))
             self.instructions[cmd](*v)
 
 
 class Today(Puzzle):
+    def parser(self):
+        """MONAD code is separated in blocks each starting with an
+        input instruction.
+        """
+        self.blocks = []
+        for line in self.input:
+            if "inp" in line:
+                self.blocks.append([])
+            self.blocks[-1].append(line)
+        print(*self.blocks, sep="\n")
+
     def part_one(self):
-        for i in range(99999999999999, 1, -1):
-            if "0" in str(i):
-                continue
-            alu = ALU(i, self.input)
-            alu.run()
-            if alu.mem["z"] == 0:
-                return i
+        """From one block to another, w is used for input and x and y
+        are set to 0. Thus, only the value of z needs to be stored
+        for next block.
+        """
+        old = {0: 0}
+        for block in self.blocks:
+            new = {}
+            for i in range(9, 0, -1):
+                for z, n in old.items():
+                    model = int(f"{n}{i}")
+                    alu = ALU(i, block, z)
+                    alu.run()
+                    if alu.mem["z"] not in new or model > new[alu.mem["z"]]:
+                        new[alu.mem["z"]] = model
+            old = new
+        return new[0]
 
     def part_two(self):
-        return super().part_two()
+        old = {0: 0}
+        for block in self.blocks:
+            new = {}
+            for i in range(1, 10):
+                for z, n in old.items():
+                    model = int(f"{n}{i}")
+                    alu = ALU(i, block, z)
+                    alu.run()
+                    if alu.mem["z"] not in new or model < new[alu.mem["z"]]:
+                        new[alu.mem["z"]] = model
+            old = new
+        return new[0]
 
-
-solutions = (None, None)
+solutions = (29989297949519, 19518121316118)
 
 if __name__ == "__main__":
-    # Today(solutions=solutions).solve()
-    t = Today()
-    print(t.__class__())
+    Today(solutions=solutions).solve()
