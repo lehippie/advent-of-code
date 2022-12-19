07bca8906e022c1e9d8064b0947e6b065ca176a3
lehippie <dahip@gmx.fr>
2022-12-19 12:11:57 +0100

    code clarity and better docstring

--- 2022/16.py
+++ 2022/16.py
@@ -19,50 +19,52 @@ def bfs(graph, current, goal):
                 frontier.append((destination, time + 1))
 
 
+def max_pressure(graph, flows, valves, minutes=30):
+    """Return maximum pressure released using <valves>."""
+    max_released = 0
+    frontier = [("AA", set(), 0, 0)]
+    while frontier:
+        position, opened, time, pressure = frontier.pop()
+        if pressure > max_released:
+            max_released = pressure
+        for valve, move_time in graph[position].items():
+            if valve in opened or valve not in valves:
+                continue
+            if (T := time + move_time) < minutes:
+                P = pressure + (minutes - T) * flows[valve]
+                frontier.append((valve, opened.union({valve}), T, P))
+    return max_released
+
+
 class Today(Puzzle):
     def parser(self):
-        self.flow = {}
+        self.flows = {}
         self.tunnels = {}
         for line in self.input:
             valve, tunnel = line.split(";")
             v = valve.split()[1]
-            self.flow[v] = int(valve.split("=")[-1])
+            self.flows[v] = int(valve.split("=")[-1])
             self.tunnels[v] = [t.replace(",", "") for t in tunnel.split()[4:]]
-
-    def max_pressure(self, valves, minutes=30):
-        """Return maximum pressure released using <valves>."""
-        max_released = 0
-        paths = [("AA", set(), 0, 0)]
-        while paths:
-            position, opened, time, pressure = paths.pop()
-            if pressure > max_released:
-                max_released = pressure
-            for valve, move_time in self.graph[position].items():
-                if valve in opened or valve not in valves:
-                    continue
-                if (T := time + move_time) < minutes:
-                    P = pressure + (minutes - T) * self.flow[valve]
-                    paths.append((valve, opened.union({valve}), T, P))
-        return max_released
 
     def part_one(self):
         """Valves with flow rate 0 are just tunnels so we only care
-        about the paths between each pair of valves with flow > 0.
+        about the paths between each pair of useful valves.
         """
-        self.valves = {v for v, f in self.flow.items() if f}
+        self.valves = {v for v, f in self.flows.items() if f}
         self.graph = defaultdict(dict)
         for v1, v2 in combinations(self.valves, 2):
             self.graph[v1][v2] = bfs(self.tunnels, v1, v2)
             self.graph[v2][v1] = self.graph[v1][v2]
         self.graph["AA"] = {v: bfs(self.tunnels, "AA", v) for v in self.valves}
-        return self.max_pressure(self.valves)
+        return max_pressure(self.graph, self.flows, self.valves)
 
     def part_two(self):
         """We need to find the better pair of disjoint sets of valves.
         For that, all paths that can be explored in 26 minutes are
         considered.
-        Only the sets of valves for these paths arekapt as key of the
-        max pressure dict to ensure we keep the best permutations.
+        For paths with same valves but not in the same order, we keep
+        the permutation leading to the max released pressure (stored
+        as frozensets to be usable as dict keys).
         """
         paths = {}
         frontier = [(["AA"], [0])]
@@ -74,7 +76,7 @@ class Today(Puzzle):
                     new_times = times + [T]
                     frontier.append((new_path, new_times))
                     valves = frozenset(new_path[1:])
-                    P = self.max_pressure(valves, 26)
+                    P = max_pressure(self.graph, self.flows, valves, 26)
                     if valves not in paths or P > paths[valves]:
                         paths[valves] = P
         return max(
