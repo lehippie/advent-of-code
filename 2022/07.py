e90b93baa590c83fbe71fda3ce670a3ad1854871
lehippie <dahip@gmx.fr>
2022-12-20 15:40:18 +0100

    Move functions out of Puzzle class

--- 2022/07.py
+++ 2022/07.py
@@ -5,8 +5,35 @@ from aoc.puzzle import Puzzle
 
 class Dir(list):
     def size(self):
-        """Recursive size calculation for list class."""
+        """Recursive size method for lists."""
         return sum(c.size() if isinstance(c, Dir) else c[0] for c in self)
+
+
+def create_tree(commands):
+    """Read commands and their outputs to construct a directory tree.
+    Absolute paths are used because directories with the same name
+    can exists in different places.
+    """
+    tree = {"root": Dir()}
+    for cmd, *output in commands:
+        if "cd" in cmd:
+            destination = cmd[3:]
+            if destination == "/":
+                cwd = "root"
+            elif destination == "..":
+                cwd = cwd[: cwd.rfind("/")]
+            else:
+                cwd = f"{cwd}/{destination}"
+        elif cmd == "ls":
+            for thing in output:
+                if thing.startswith("dir"):
+                    dname = f"{cwd}/{thing[4:]}"
+                    tree[dname] = Dir()
+                    tree[cwd].append(tree[dname])
+                else:
+                    size, fname = thing.split()
+                    tree[cwd].append((int(size), fname))
+    return tree
 
 
 class Today(Puzzle):
@@ -15,34 +42,9 @@ class Today(Puzzle):
         self.commands = "|".join(self.input).split("$ ")
         self.commands = [c.rstrip("|").split("|") for c in self.commands if c]
 
-    def create_tree(self):
-        """Read commands and outputs to construct a tree. Absolute
-        paths are used because directories with the same name can
-        exists in different places.
-        """
-        self.tree = {"/": Dir()}
-        for cmd, *output in self.commands:
-            if "cd" in cmd:
-                destination = cmd[3:]
-                if destination == "/":
-                    cwd = destination
-                elif destination == "..":
-                    cwd = cwd[: cwd.rfind("/")]
-                else:
-                    cwd = f"{cwd}/{destination}"
-            elif cmd == "ls":
-                for thing in output:
-                    if thing.startswith("dir"):
-                        dname = f"{cwd}/{thing[4:]}"
-                        self.tree[dname] = Dir()
-                        self.tree[cwd].append(self.tree[dname])
-                    else:
-                        size, fname = thing.split()
-                        self.tree[cwd].append((int(size), fname))
-
     def part_one(self):
-        self.create_tree()
-        self.sizes = [d.size() for d in self.tree.values()]
+        tree = create_tree(self.commands)
+        self.sizes = [d.size() for d in tree.values()]
         return sum(s for s in self.sizes if s <= 100000)
 
     def part_two(self):
--- 2022/15.py
+++ 2022/15.py
@@ -4,6 +4,25 @@ import re
 from aoc.puzzle import Puzzle
 
 
+def scanned_ranges(sensors, y):
+    """Return contiguous scanned ranges in row <y> by listing
+    ranges of each scanner and removing intersections.
+    """
+    ranges = [
+        [sx - radius + dy, sx + radius - dy]
+        for (sx, sy), radius in sensors.items()
+        if (dy := abs(y - sy)) <= radius
+    ]
+    ranges.sort()
+    unions = [ranges.pop(0)]
+    for start, stop in ranges:
+        if start <= unions[-1][1]:
+            unions[-1][1] = max(stop, unions[-1][1])
+        else:
+            unions.append([start, stop])
+    return unions
+
+
 class Today(Puzzle):
     def parser(self):
         self.sensors = {}
@@ -13,32 +32,14 @@ class Today(Puzzle):
             self.sensors[(sx, sy)] = abs(sx - bx) + abs(sy - by)
             self.beacons.add((bx, by))
 
-    def scanned_ranges(self, y):
-        """Return contiguous scanned positions in row <y>."""
-        # List scanners' ranges
-        ranges = []
-        for (sx, sy), radius in self.sensors.items():
-            if (dy := abs(y - sy)) <= radius:
-                xmin, xmax = sx - radius + dy, sx + radius - dy
-                ranges.append([xmin, xmax])
-        # Remove intersections
-        ranges.sort()
-        unions = [ranges.pop(0)]
-        for start, stop in ranges:
-            if start <= unions[-1][1]:
-                unions[-1][1] = max(stop, unions[-1][1])
-            else:
-                unions.append([start, stop])
-        return unions
-
     def part_one(self, y=2000000):
-        scanned = self.scanned_ranges(y)
+        scan = scanned_ranges(self.sensors, y)
         n_beacons = sum(by == y for _, by in self.beacons)
-        return sum(M - m + 1 for m, M in scanned) - n_beacons
+        return sum(M - m + 1 for m, M in scan) - n_beacons
 
     def part_two(self, ymax=4000000):
         for y in range(ymax, -1, -1):
-            if len(s := self.scanned_ranges(y)) == 2:
+            if len(s := scanned_ranges(self.sensors, y)) == 2:
                 return 4000000 * (s[0][1] + 1) + y
 
 
