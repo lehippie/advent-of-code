0cdf97860a0b6df2efaae61d2673c2c9d4f5af51
lehippie <dahip@gmx.fr>
2023-12-02 18:45:53 +0100

    2023.02

---
+++ 2023/02.py
@@ -0,0 +1,35 @@
+"""--- Day 2: Cube Conundrum ---"""
+
+import re
+from aoc.puzzle import Puzzle
+
+
+def is_possible(game, max_cubes):
+    for color, maxi in max_cubes.items():
+        max_seen = max(map(int, re.findall(rf"(\d+) {color}", game)))
+        if max_seen > maxi:
+            return 0
+    return int(game.split(":")[0].split(" ")[-1])
+
+
+def power(game):
+    p = 1
+    for color in ("red", "green", "blue"):
+        max_seen = max(map(int, re.findall(rf"(\d+) {color}", game)))
+        p *= max_seen
+    return p
+
+
+class Today(Puzzle):
+    def part_one(self):
+        max_cubes = {"red": 12, "green": 13, "blue": 14}
+        return sum(is_possible(game, max_cubes) for game in self.input)
+
+    def part_two(self):
+        return sum(power(game) for game in self.input)
+
+
+solutions = (2237, 66681)
+
+if __name__ == "__main__":
+    Today(solutions=solutions).solve()
