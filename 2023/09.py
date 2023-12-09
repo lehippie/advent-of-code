4a67d031efdd04831b407403359c37904e8ece00
lehippie <dahip@gmx.fr>
2023-12-09 11:50:27 +0100

    2023.09

---
+++ 2023/09.py
@@ -0,0 +1,38 @@
+"""--- Day 9: Mirage Maintenance ---"""
+
+from aoc.puzzle import Puzzle
+
+
+def prediction(history):
+    pred = history[-1]
+    while history[-1] != 0:
+        history = [h2 - h1 for h1, h2 in zip(history, history[1:])]
+        pred += history[-1]
+    return pred
+
+
+def backward_prediction(history):
+    pred = history[0]
+    step = 0
+    while any(history):
+        step += 1
+        history = [h2 - h1 for h1, h2 in zip(history, history[1:])]
+        pred += -history[0] if step % 2 else history[0]
+    return pred
+
+
+class Today(Puzzle):
+    def parser(self):
+        self.histories = [list(map(int, l.split())) for l in self.input]
+
+    def part_one(self):
+        return sum(prediction(history) for history in self.histories)
+
+    def part_two(self):
+        return sum(backward_prediction(history) for history in self.histories)
+
+
+solutions = (1955513104, 1131)
+
+if __name__ == "__main__":
+    Today(solutions=solutions).solve()
