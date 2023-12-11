5af9f806c3b1811cb749e8afaa8a73a3fed661a0
lehippie <dahip@gmx.fr>
2023-12-11 10:39:07 +0100

    2023.10 better doc and naming

--- 2023/10.py
+++ 2023/10.py
@@ -14,20 +14,20 @@ PIPES = {
 
 class Today(Puzzle):
     def parser(self):
-        self.sketch = {}  # Full sketch with complex numbers
+        self.map = {}  # Map indexed with complex numbers
         self.pipes = {}  # Pipes with their connections
         for r, row in enumerate(self.input):
             for c, pipe in enumerate(row):
                 position = r + c * 1j
-                self.sketch[position] = pipe
+                self.map[position] = pipe
                 if pipe == "S":
                     self.start = position
                 elif pipe in PIPES:
                     self.pipes[position] = [position + p for p in PIPES[pipe]]
         # Add the starting position to the pipes
         self.pipes[self.start] = [k for k, v in self.pipes.items() if self.start in v]
-        # Replace the "S" in the sketch by the correct pipe shape
-        self.sketch[self.start] = next(
+        # Replace the "S" in the map by the correct pipe shape
+        self.map[self.start] = next(
             pipe
             for pipe, links in PIPES.items()
             if set(p - self.start for p in self.pipes[self.start]) == set(links)
@@ -35,7 +35,7 @@ class Today(Puzzle):
 
     def part_one(self):
         """BFS applied to the pipes from "S". Farthest point is the
-        floor of the length of the loop divided by 2.
+        floor of the half-length of the loop.
         """
         frontier = [self.start]
         self.loop = {self.start}
@@ -49,25 +49,25 @@ class Today(Puzzle):
 
     def part_two(self):
         """For each line, we start on the outside of the loop and
-        alternate between inside/outise each time we cross pipes
-        of the loop (|, L+7 or F+J).
+        alternate being inside/outside each time we cross a full
+        vertical pipe of the loop (|, L+7 or F+J).
         """
         enclosed = 0
         for r in range(len(self.input)):
             crossed = 0
             on_loop = ""
             for c in range(len(self.input[0])):
-                pos = r + c * 1j
-                if pos in self.loop:
-                    if self.sketch[pos] in {"L", "F"}:
-                        on_loop = self.sketch[pos]
+                position = r + c * 1j
+                if position in self.loop:
+                    if self.map[position] in {"L", "F"}:
+                        on_loop = self.map[position]
                     elif (
-                        self.sketch[pos] == "|"
-                        or (on_loop == "L" and self.sketch[pos] == "7")
-                        or (on_loop == "F" and self.sketch[pos] == "J")
+                        self.map[position] == "|"
+                        or (on_loop == "L" and self.map[position] == "7")
+                        or (on_loop == "F" and self.map[position] == "J")
                     ):
                         crossed += 1
-                elif pos not in self.loop and crossed % 2:
+                elif position not in self.loop and crossed % 2:
                     enclosed += 1
         return enclosed
 
