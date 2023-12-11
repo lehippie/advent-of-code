64327d87a67338ee9f544ee2ba379a5e40a85543
lehippie <dahip@gmx.fr>
2023-12-11 10:32:17 +0100

    2023.11.part2 v2

--- 2023/11.py
+++ 2023/11.py
@@ -17,13 +17,16 @@ class Today(Puzzle):
                     self.empty_cols.discard(c)
 
     def part_one(self, expansion=1):
-        distances_sum = 0
+        distances = 0
         for (r1, c1), (r2, c2) in combinations(self.galaxies, 2):
-            d = abs(r2 - r1) + abs(c2 - c1)
-            d += expansion * sum(min(r1, r2) < r < max(r1, r2) for r in self.empty_rows)
-            d += expansion * sum(min(c1, c2) < c < max(c1, c2) for c in self.empty_cols)
-            distances_sum += d
-        return distances_sum
+            rm, rM = min(r1, r2), max(r1, r2)
+            cm, cM = min(c1, c2), max(c1, c2)
+            distances += rM - rm + cM - cm
+            distances += expansion * (
+                len(self.empty_rows.intersection(range(rm, rM)))
+                + len(self.empty_cols.intersection(range(cm, cM)))
+            )
+        return distances
 
     def part_two(self):
         return self.part_one(expansion=999999)
