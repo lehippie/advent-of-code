6c7cba25459019363e04d107969b49233ff81d75
lehippie <dahip@gmx.fr>
2024-11-04 12:37:49 +0100

    [timings] Create table in readme based on puzzle timings

--- aoc/timings.py
+++ aoc/timings.py
@@ -6,24 +6,26 @@ This script analyze the solutions.json and timings.json files and
 add to the latter the solved puzzles not yet timed.
 
 Usage:
-    {name} [--erase]
+    {name} [--erase] [--update-readme]
     {name} --help
 
 Options:
-    -e, --erase         Recalculate previous timings.
-    -h, --help          Show this help.
+    -e, --erase             Recalculate previous timings
+    -u, --update-readme     Update README.md's table
+    -h, --help              Show this help
 """
 
 import json
 import re
 import sys
+from datetime import date
 from importlib import import_module
 from pathlib import Path
 from timeit import timeit
 
 from docopt import docopt
 
-from aoc import SOLUTIONS, TIMINGS
+from aoc import ROOT, SOLUTIONS, TIMINGS
 
 
 def write_timing_file(timings: dict):
@@ -40,18 +42,17 @@ def write_timing_file(timings: dict):
         f.write(s)
 
 
-def get_time(year, day, solutions):
+def puzzle_timing(year, day, solutions):
     """Calculate the timing of a puzzle."""
-    day = f"{day:>02}"
-    puzzle = import_module(f"{year}.{day}").Today()
+    puzzle = import_module(f"{year}.{day:>02}").Today()
     return [
         round(1000 * timeit(part, number=1), 3)
         for part in zip([puzzle.part_one, puzzle.part_two], solutions)
     ]
 
 
-def get_all_times(erase=False):
-    """Calculate the timing of all fully solved puzzle."""
+def get_timings(erase):
+    """Calculate the timings of all fully solved puzzle."""
     with open(SOLUTIONS) as f:
         solutions = json.load(f)
     with open(TIMINGS) as f:
@@ -69,14 +70,54 @@ def get_all_times(erase=False):
                 print(f"{day} skipped (incomplete)")
                 continue
             if not timings[year][day] or erase:
-                timings[year][day] = get_time(year, day, day_solutions)
+                timings[year][day] = puzzle_timing(year, day, day_solutions)
                 print(f"{day}: {timings[year][day]} ms")
 
     return timings
 
 
+def get_emoji(timing):
+    if timing < 1:
+        return ":zap:"
+    if timing < 1000:
+        return ":green_square:"
+    if timing < 5000:
+        return ":blue_square:"
+    if timing < 60000:
+        return ":orange_square:"
+    if timing < 300000:
+        return ":red_square:"
+    return ":skull:"
+
+
+def update_readme(timings):
+    header = (
+        "# advent-of-code\n\nAdvent of Code solutions in python 3.10\n\n"
+        ":zap: < 1 ms&emsp;:green_square: < 1 s&emsp;:blue_square: < 5 s&emsp;"
+        ":orange_square: < 1 min&emsp;:red_square: < 5 min&emsp;:skull: > 5 min&emsp;"
+        ":x: unsolved day\n\n"
+    )
+    footer = f"\n\n_(last update: {date.today().isoformat()})_\n"
+    years = sorted(timings)
+    table = ["||" + "|".join(years) + "|"]
+    table.append("|:---:|" + "".join(":---:|" for _ in range(len(years))))
+    for day in range(1, 26):
+        table.append(f"|{day}|")
+        for year in years:
+            times = timings[year][f"{day}"]
+            if times:
+                table[-1] += " ".join(get_emoji(t) for t in times) + "|"
+            else:
+                table[-1] += ":x:|"
+
+    readme = ROOT / "README.md"
+    readme.write_text(header + "\n".join(table) + footer)
+
+
 if __name__ == "__main__":
     args = docopt(__doc__.format(name=Path(__file__).name))
-    sys.path.append("./2019")  # To allow local import of intcode computer
-    times = get_all_times(args["--erase"])
-    write_timing_file(times)
+    sys.path.append("./2019")  # Allow local import of intcode computer
+    timings = get_timings(args["--erase"])
+    write_timing_file(timings)
+    if args["--update-readme"]:
+        update_readme(timings)
