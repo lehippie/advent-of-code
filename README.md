5efa529c2080b4e22ae05047574aa02dbc59b79d
lehippie <dahip@gmx.fr>
2024-11-05 19:57:49 +0100

    [timings] Calculate timing when only part1 is solved

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Advent of Code solutions in python 3.10
 
-:zap: < 1 ms&emsp;:green_square: < 1 s&emsp;:blue_square: < 5 s&emsp;:orange_square: < 1 min&emsp;:red_square: < 5 min&emsp;:skull: > 5 min&emsp;:x: unsolved day
+:zap: < 1 ms&emsp;:green_square: < 1 s&emsp;:blue_square: < 5 s&emsp;:orange_square: < 1 min&emsp;:red_square: < 5 min&emsp;:skull: > 5 min&emsp;:x: unsolved
 
 ||2015|2016|2017|2018|2019|2020|2021|2022|2023|
 |:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|
@@ -22,14 +22,14 @@ Advent of Code solutions in python 3.10
 |14|:zap: :green_square:|:x:|:x:|:x:|:zap: :green_square:|:zap: :green_square:|:zap: :green_square:|:green_square: :green_square:|:green_square: :blue_square:|
 |15|:green_square: :green_square:|:x:|:x:|:x:|:blue_square: :zap:|:zap: :blue_square:|:green_square: :green_square:|:zap: :orange_square:|:green_square: :green_square:|
 |16|:zap: :zap:|:x:|:x:|:x:|:green_square: :orange_square:|:zap: :green_square:|:zap: :zap:|:green_square: :orange_square:|:green_square: :blue_square:|
-|17|:green_square: :zap:|:x:|:x:|:x:|:green_square: :green_square:|:green_square: :green_square:|:zap: :green_square:|:x:|:green_square: :blue_square:|
+|17|:green_square: :zap:|:x:|:x:|:x:|:green_square: :green_square:|:green_square: :green_square:|:zap: :green_square:|:green_square: :x:|:green_square: :blue_square:|
 |18|:blue_square: :blue_square:|:x:|:x:|:x:|:x:|:green_square: :green_square:|:green_square: :blue_square:|:green_square: :green_square:|:green_square: :zap:|
 |19|:zap: :zap:|:x:|:x:|:x:|:x:|:green_square: :orange_square:|:blue_square: :zap:|:x:|:zap: :green_square:|
 |20|:orange_square: :orange_square:|:x:|:x:|:x:|:x:|:green_square: :green_square:|:green_square: :blue_square:|:green_square: :blue_square:|:green_square: :green_square:|
-|21|:green_square: :green_square:|:x:|:x:|:x:|:x:|:zap: :zap:|:zap: :green_square:|:green_square: :green_square:|:x:|
-|22|:green_square: :green_square:|:x:|:x:|:x:|:x:|:zap: :blue_square:|:green_square: :blue_square:|:green_square: :green_square:|:x:|
-|23|:green_square: :zap:|:x:|:x:|:x:|:x:|:zap: :orange_square:|:green_square: :green_square:|:green_square: :orange_square:|:x:|
-|24|:green_square: :green_square:|:x:|:x:|:x:|:x:|:green_square: :green_square:|:skull: :skull:|:x:|:x:|
+|21|:green_square: :green_square:|:x:|:x:|:x:|:x:|:zap: :zap:|:zap: :green_square:|:green_square: :green_square:|:green_square: :x:|
+|22|:green_square: :green_square:|:x:|:x:|:x:|:x:|:zap: :blue_square:|:green_square: :blue_square:|:green_square: :green_square:|:green_square: :x:|
+|23|:green_square: :zap:|:x:|:x:|:x:|:x:|:zap: :orange_square:|:green_square: :green_square:|:green_square: :orange_square:|:orange_square: :x:|
+|24|:green_square: :green_square:|:x:|:x:|:x:|:x:|:green_square: :green_square:|:skull: :skull:|:x:|:green_square: :x:|
 |25|:blue_square:|:x:|:x:|:x:|:x:|:blue_square:|:blue_square:|:zap:|:zap:|
 
-_(last update: 2024-11-04)_
+_(last update: 2024-11-05)_
--- aoc/timings.py
+++ aoc/timings.py
@@ -4,14 +4,14 @@
 
 This script analyze the solutions.json and timings.json files and
 add to the latter the solved puzzles not yet timed.
+README.md's table is also updated with latest timings.
 
 Usage:
-    {name} [--erase] [--update-readme]
+    {name} [--erase]
     {name} --help
 
 Options:
     -e, --erase             Recalculate previous timings
-    -u, --update-readme     Update README.md's table
     -h, --help              Show this help
 """
 
@@ -21,7 +21,7 @@ import sys
 from datetime import date
 from importlib import import_module
 from pathlib import Path
-from timeit import timeit
+from time import process_time
 
 from docopt import docopt
 
@@ -47,10 +47,13 @@ def write_timing_file(timings: dict):
 def puzzle_timing(year, day, solutions):
     """Calculate the timing of a puzzle."""
     puzzle = import_module(f"{year}.{day:>02}").Today()
-    return [
-        round(1000 * timeit(part, number=1), 3)
-        for part in zip([puzzle.part_one, puzzle.part_two], solutions)
-    ]
+    timing = []
+    for part, solution in zip((puzzle.part_one, puzzle.part_two), solutions):
+        start = process_time()
+        answer = part()
+        part_duration = round(1000 * (process_time() - start), 3)
+        timing.append(part_duration if answer == solution else None)
+    return timing
 
 
 def get_timings(erase):
@@ -61,24 +64,26 @@ def get_timings(erase):
         timings = json.load(f)
 
     for year, year_solutions in solutions.items():
-        print(year)
         if year not in timings:
             timings[year] = {}
 
         for day, day_solutions in year_solutions.items():
             if day not in timings[year]:
-                timings[year][day] = []
-            if None in day_solutions:
-                print(f"{day} skipped (incomplete)")
+                timings[year][day] = [None for _ in day_solutions]
+            if not any(day_solutions):
                 continue
-            if not timings[year][day] or erase:
+            if erase or any(
+                s and not t for s, t in zip(day_solutions, timings[year][day])
+            ):
                 timings[year][day] = puzzle_timing(year, day, day_solutions)
-                print(f"{day}: {timings[year][day]} ms")
+                print(f"{year}-{day}: {timings[year][day]} ms")
 
     return timings
 
 
 def get_emoji(timing):
+    if timing is None:
+        return ":x:"
     if timing < 1:
         return ":zap:"
     if timing < 1000:
@@ -97,17 +102,17 @@ def update_readme(timings):
         "# advent-of-code\n\nAdvent of Code solutions in python 3.10\n\n"
         ":zap: < 1 ms&emsp;:green_square: < 1 s&emsp;:blue_square: < 5 s&emsp;"
         ":orange_square: < 1 min&emsp;:red_square: < 5 min&emsp;:skull: > 5 min&emsp;"
-        ":x: unsolved day\n\n"
+        ":x: unsolved\n\n"
     )
     footer = f"\n\n_(last update: {date.today().isoformat()})_\n"
     years = sorted(timings)
     table = ["||" + "|".join(years) + "|"]
-    table.append("|:---:|" + "".join(":---:|" for _ in range(len(years))))
+    table.append("|:---:|" + "".join(":---:|" for _ in years))
     for day in range(1, 26):
         table.append(f"|{day}|")
         for year in years:
             times = timings[year][f"{day}"]
-            if times:
+            if any(times):
                 table[-1] += " ".join(get_emoji(t) for t in times) + "|"
             else:
                 table[-1] += ":x:|"
@@ -121,5 +126,4 @@ if __name__ == "__main__":
     sys.path.append("./2019")  # Allow local import of intcode computer
     timings = get_timings(args["--erase"])
     write_timing_file(timings)
-    if args["--update-readme"]:
-        update_readme(timings)
+    update_readme(timings)
